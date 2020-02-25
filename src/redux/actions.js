ecc9a5f0a0e1ce409908bd5dcbc5a48ddd4c3f92
TripKings <antonio.cicchese@gmail.com>
2020-02-25 15:48:27 -0600

    got stuck on removing the cars

---
+++ src/redux/actions.js
@@ -0,0 +1,16 @@
+const addCar = (car) => {
+    return {
+        type: "ADD_CAR",
+        value: car
+    }
+}
+
+const removeCar = (index) => {
+    return {
+        type: "REMOVE_CAR",
+        value: index
+
+    }
+}
+
+export {addCar, removeCar}
